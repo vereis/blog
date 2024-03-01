92eb10322c1bcb1d90e11e3d9aa0f6d871304604
Chris Bailey <me@cbailey.co.uk>
2024-03-01 08:50:08 +0000

    Fix formatting

--- backend/apps/blog/priv/repo/migrations/20230607110334_create_posts.exs
+++ backend/apps/blog/priv/repo/migrations/20230607110334_create_posts.exs
@@ -4,19 +4,19 @@ defmodule Blog.Repo.Migrations.CreatePosts do
   def change do
     create table(:posts) do
       # Core content
-      add :title, :string, null: false
-      add :body, :text, default: ""
-      add :raw_body, :text, default: ""
+      add(:title, :string, null: false)
+      add(:body, :text, default: "")
+      add(:raw_body, :text, default: "")
 
       # Metadata
-      add :slug, :string, null: false
-      add :reading_time_minutes, :integer, default: 0
-      add :is_draft, :boolean, default: true
-      add :published_at, :utc_datetime
+      add(:slug, :string, null: false)
+      add(:reading_time_minutes, :integer, default: 0)
+      add(:is_draft, :boolean, default: true)
+      add(:published_at, :utc_datetime)
 
-      timestamps
+      timestamps()
     end
 
-    create unique_index(:posts, [:slug])
+    create(unique_index(:posts, [:slug]))
   end
 end
--- backend/apps/blog/priv/repo/migrations/20230607111241_create_tags.exs
+++ backend/apps/blog/priv/repo/migrations/20230607111241_create_tags.exs
@@ -4,13 +4,13 @@ defmodule Blog.Repo.Migrations.CreateTags do
   def change do
     create table(:tags) do
       add :label, :string, null: false
-      timestamps
+      timestamps()
     end
 
     create table(:posts_tags) do
       add :post_id, references(:posts, on_delete: :delete_all), null: false
       add :tag_id, references(:tags, on_delete: :delete_all), null: false
-      timestamps
+      timestamps()
     end
 
     create unique_index(:tags, [:label])
