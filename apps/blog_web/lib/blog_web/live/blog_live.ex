4ee167b80dec0d34c7439d4bc1a57765287d1e47
vereis <me@vereis.com>
2025-03-27 13:19:55 +0000

    Refactor CSS and update HTML for better semantics

--- apps/blog_web/lib/blog_web/live/blog_live.ex
+++ apps/blog_web/lib/blog_web/live/blog_live.ex
@@ -7,104 +7,104 @@ defmodule BlogWeb.BlogLive do
   alias Blog.Posts
 
   # TODO: source these from some `Blog` context.
-  @projects [
-    %{
-      name: "Neovim Config",
-      tags: [%{label: "neovim"}, %{label: "lua"}],
-      url: "https://github.com/vereis/nix-config/tree/master/modules/home/neovim/lua",
-      description: """
-      Personal Neovim configuration based on Lazy, with LSP support, Treesitter, and custom Lua plugins.
-      """
-    },
-    %{
-      name: "Nix Config",
-      tags: [%{label: "nix"}, %{label: "nixos"}],
-      url: "https://github.com/vereis/nix-config",
-      description: """
-      Personal Nix configuration utilizing flakes and building regularly for Windows (WSL), Linux, and MacOS.
-      """
-    },
-    %{
-      name: "Toggle",
-      tags: [%{label: "elixir"}, %{label: "ecto"}],
-      url: "https://github.com/vereis/toggle",
-      description: """
-      Minimalistic and stupid-simple Feature Flagging Library for Elixir, with cache support and hooking capabilities.
-      """
-    },
-    %{
-      name: "Cinema",
-      tags: [%{label: "elixir"}, %{label: "ecto"}, %{label: "oban"}],
-      url: "https://github.com/vereis/cinema",
-      description: """
-      Framework for defining Incremental Materialized Views in raw Elixir and Ecto, with support for recursive/dependant views.
-      """
-    },
-    %{
-      name: "Sibyl",
-      tags: [%{label: "elixir"}, %{label: "telemetry"}],
-      url: "https://github.com/vetspire/sibyl",
-      description: """
-      Zero runtime cost telemetry and distributed tracing with custom plugins, library support, leveraging standard telemetry events.
-      """
-    },
-    %{
-      name: "Endo",
-      tags: [%{label: "elixir"}, %{label: "ecto"}],
-      url: "https://github.com/vetspire/endo",
-      description: """
-      Database reflection tool with fluent, composable API for Postgres and SQLite.
-      """
-    },
-    %{
-      name: "Ecto Middleware",
-      tags: [%{label: "elixir"}, %{label: "ecto"}],
-      url: "https://github.com/vereis/ecto_middleware",
-      description: """
-      Generic middleware implementation for Ecto, allowing for easy customization and extension of Ecto's standard Repo interface.
-      """
-    },
-    %{
-      name: "Ecto Hooks",
-      tags: [%{label: "elixir"}, %{label: "ecto"}],
-      url: "https://github.com/vereis/ecto_hooks",
-      description: """
-      Library which extends Ecto Schemas with the ability to automatically execute before or after hooks on Repo callbacks.
-      """
-    },
-    %{
-      name: "Ecto Model",
-      tags: [%{label: "elixir"}, %{label: "ecto"}],
-      url: "https://github.com/vetspire/ecto_model",
-      description: """
-      Collection of various Ecto extensions and utilities to make working with Ecto more enjoyable, including a query building abstraction and hooks.
-      """
-    },
-    %{
-      name: "Monarch",
-      tags: [%{label: "elixir"}, %{label: "ecto"}, %{label: "migrations"}],
-      url: "https://hexdocs.pm/monarch/Monarch.html",
-      description: """
-      A simple Oban powered framework for defining and running long running, large data backfill tasks asynchronously across your cluster.
-      """
-    },
-    %{
-      name: "Jarlang",
-      tags: [%{label: "erlang"}, %{label: "compiler"}],
-      url: "https://github.com/vereis/jarlang",
-      description: """
-      (Archived) Erlang to ES5 compiler, written in Erlang and absolutely, definitely not suitable for production use.
-      """
-    },
-    %{
-      name: "Blog",
-      tags: [%{label: "elixir"}, %{label: "liveview"}],
-      url: "https://github.com/vereis/blog",
-      description: """
-      My personal blog and playground for Phoenix LiveView and other technologies.
-      """
-    }
-  ]
+  @projects Enum.reverse([
+              %{
+                name: "Neovim Config",
+                tags: [%{label: "neovim"}, %{label: "lua"}],
+                url: "https://github.com/vereis/nix-config/tree/master/modules/home/neovim/lua",
+                description: """
+                Personal Neovim configuration based on Lazy, with LSP support, Treesitter, and custom Lua plugins.
+                """
+              },
+              %{
+                name: "Nix Config",
+                tags: [%{label: "nix"}, %{label: "nixos"}],
+                url: "https://github.com/vereis/nix-config",
+                description: """
+                Personal Nix configuration utilizing flakes and building regularly for Windows (WSL), Linux, and MacOS.
+                """
+              },
+              %{
+                name: "Toggle",
+                tags: [%{label: "elixir"}, %{label: "ecto"}],
+                url: "https://github.com/vereis/toggle",
+                description: """
+                Minimalistic and stupid-simple Feature Flagging Library for Elixir, with cache support and hooking capabilities.
+                """
+              },
+              %{
+                name: "Cinema",
+                tags: [%{label: "elixir"}],
+                url: "https://github.com/vereis/cinema",
+                description: """
+                Framework for defining Incremental Materialized Views in raw Elixir and Ecto, with support for recursive/dependant views.
+                """
+              },
+              %{
+                name: "Sibyl",
+                tags: [%{label: "elixir"}, %{label: "telemetry"}],
+                url: "https://github.com/vetspire/sibyl",
+                description: """
+                Zero runtime cost telemetry and distributed tracing with custom plugins, library support, leveraging standard telemetry events.
+                """
+              },
+              %{
+                name: "Endo",
+                tags: [%{label: "elixir"}, %{label: "ecto"}],
+                url: "https://github.com/vetspire/endo",
+                description: """
+                Database reflection tool with fluent, composable API for Postgres and SQLite.
+                """
+              },
+              %{
+                name: "Ecto Middleware",
+                tags: [%{label: "elixir"}, %{label: "ecto"}],
+                url: "https://github.com/vereis/ecto_middleware",
+                description: """
+                Generic middleware implementation for Ecto, allowing for easy customization and extension of Ecto's standard Repo interface.
+                """
+              },
+              %{
+                name: "Ecto Hooks",
+                tags: [%{label: "elixir"}, %{label: "ecto"}],
+                url: "https://github.com/vereis/ecto_hooks",
+                description: """
+                Library which extends Ecto Schemas with the ability to automatically execute before or after hooks on Repo callbacks.
+                """
+              },
+              %{
+                name: "Ecto Model",
+                tags: [%{label: "elixir"}, %{label: "ecto"}],
+                url: "https://github.com/vetspire/ecto_model",
+                description: """
+                Collection of various Ecto extensions and utilities to make working with Ecto more enjoyable, including a query building abstraction and hooks.
+                """
+              },
+              %{
+                name: "Monarch",
+                tags: [%{label: "elixir"}, %{label: "migrations"}],
+                url: "https://hexdocs.pm/monarch/Monarch.html",
+                description: """
+                A simple Oban powered framework for defining and running long running, large data backfill tasks asynchronously across your cluster.
+                """
+              },
+              %{
+                name: "Jarlang",
+                tags: [%{label: "erlang"}, %{label: "compiler"}],
+                url: "https://github.com/vereis/jarlang",
+                description: """
+                (Archived) Erlang to ES5 compiler, written in Erlang and absolutely, definitely not suitable for production use.
+                """
+              },
+              %{
+                name: "Blog",
+                tags: [%{label: "elixir"}, %{label: "liveview"}],
+                url: "https://github.com/vereis/blog",
+                description: """
+                My personal blog and playground for Phoenix LiveView and other technologies.
+                """
+              }
+            ])
 
   @impl Phoenix.LiveView
   def mount(params, _session, socket) do
@@ -196,28 +196,71 @@ defmodule BlogWeb.BlogLive do
         <main>
           <div>
             <h1>
-              All Projects <%= if @tag, do: "tagged #{@tag}", else: "" %>
+              All Projects <%= if @tag, do: "(##{@tag})", else: "" %>
             </h1>
             <%= if @tag do %>
-              <a phx-click="projects">(clear)</a>
+              <a phx-click="projects">x</a>
             <% end %>
           </div>
           <div>
-            <%= for project <- @projects, is_nil(@tag) or Enum.any?(project.tags, & &1.label == @tag) do %>
-              <div class="project">
-                <a class="project_name" href={project.url}>
-                  &nbsp;<%= project.name %>
-                </a>
-                <div class="project_tags">
-                  <%= for tag <- project.tags do %>
-                    <span class="project_tag" phx-click="proj-tag" phx-value-tag={tag.label}>
-                      <%= tag.label %>
-                    </span>
-                  <% end %>
+            <article>
+              <p>
+                Here is a list of open-source projects, libraries, tools, and configs I've created.
+              </p>
+              <p>
+                You may be interested in a more complete list of repositories I've contributed to on
+                <a href="https://github.com/vereis">GitHub </a>
+              </p>
+              <p>Unless otherwise noted, all projects are `MIT` Licensed.</p>
+
+              <div class="component-container">
+                <label class="search-container hidden">
+                  <span>search :: ~ >></span>
+                  <span class="input-container">
+                    <input
+                      type="text"
+                      onInput="this.parentNode.dataset.value = this.value"
+                      size="1"
+                      placeholder=""
+                    />
+                  </span>
+                </label>
+
+                <div>
+                  tags:
+                  <div class="tags">
+                    <%= for tag <- Enum.flat_map(@projects, & &1.tags) |> Enum.uniq() |> Enum.sort() do %>
+                      <.button phx-click="proj-tag" phx-value-tag={tag.label}>
+                        <%= "#" <> tag.label %>
+                      </.button>
+                    <% end %>
+                    <%= if @tag do %>
+                      <a phx-click="projects">(clear)</a>
+                    <% end %>
+                  </div>
                 </div>
-                <p class="project_description"><%= project.description %></p>
               </div>
-            <% end %>
+            </article>
+            <div class="projects">
+              <%= for {project, idx} <- Enum.reverse(Enum.with_index(@projects)), is_nil(@tag) or Enum.any?(project.tags, & &1.label == @tag) do %>
+                <div class="project">
+                  <div class="project-id"><%= "##{idx}" %></div>
+                  <div class="project-title-container">
+                    <a class="project-name" href={project.url}>
+                      <%= project.name %>
+                    </a>
+                    <p class="project-description"><%= project.description %></p>
+                  </div>
+                  <div class="tags">
+                    <%= for tag <- project.tags do %>
+                      <span class="tag" phx-click="proj-tag" phx-value-tag={tag.label}>
+                        <%= "#" <> tag.label %>
+                      </span>
+                    <% end %>
+                  </div>
+                </div>
+              <% end %>
+            </div>
           </div>
         </main>
       <% end %>
@@ -229,7 +272,7 @@ defmodule BlogWeb.BlogLive do
               Blog Posts <%= if @tag, do: "(##{@tag})", else: "" %>
             </h1>
             <%= if @tag do %>
-              <a class="show-all" phx-click="posts">x</a>
+              <a phx-click="posts">x</a>
             <% end %>
           </div>
           <article>
@@ -238,9 +281,21 @@ defmodule BlogWeb.BlogLive do
             </p>
             <p>Views expressed are my own and do not represent the views of my employer.</p>
             <p>You can filter by tags or search for a specific post below.</p>
-            <div class="metadata">
+            <div class="component-container">
+              <label class="search-container hidden">
+                <span>search :: ~ >></span>
+                <span class="input-container">
+                  <input
+                    type="text"
+                    onInput="this.parentNode.dataset.value = this.value"
+                    size="1"
+                    placeholder=""
+                  />
+                </span>
+              </label>
+
               <div>
-                Tags:
+                tags:
                 <div class="tags">
                   <%= for tag <- Enum.flat_map(@posts, & &1.tags) |> Enum.uniq() |> Enum.sort() do %>
                     <.button phx-click="tag" phx-value-tag={tag.label}>
@@ -248,25 +303,26 @@ defmodule BlogWeb.BlogLive do
                     </.button>
                   <% end %>
                   <%= if @tag do %>
-                    <a class="show-all" phx-click="posts">(clear)</a>
+                    <a phx-click="posts">(clear)</a>
                   <% end %>
                 </div>
               </div>
             </div>
           </article>
+
           <div class="posts">
             <%= for post <- @posts, is_nil(@tag) or Enum.any?(post.tags, & &1.label == @tag), not @is_release? or not post.is_draft do %>
               <div class="post" phx-click="post" phx-value-post={post.slug}>
-                <div class="post_id"><%= "##{post.id}" %></div>
-                <div class="post_title_container">
-                  <div class="post_title"><%= post.title %></div>
-                  <div class="post_reading_time">
+                <div class="post-id"><%= "##{post.id}" %></div>
+                <div class="post-title-container">
+                  <div class="post-title"><%= post.title %></div>
+                  <div class="post-reading-time">
                     <%= Calendar.strftime(post.published_at, "%B %d %Y, %H:%M:%S") %>
                   </div>
                 </div>
-                <div class="post_tags">
+                <div class="tags">
                   <%= for tag <- post.tags do %>
-                    <span class="post_tag" phx-click="tag" phx-value-tag={tag.label}>
+                    <span class="tag" phx-click="tag" phx-value-tag={tag.label}>
                       <%= "#" <> tag.label %>
                     </span>
                   <% end %>
@@ -279,47 +335,27 @@ defmodule BlogWeb.BlogLive do
 
       <%= if @live_action in [:show_post, :home] do %>
         <main>
-          <div>
-            <%= if is_nil(@post) do %>
-              <h1>Ut-oh !!</h1>
-            <% else %>
+          <div class="post-metadata">
+            <div class="post-title">
               <h1><%= @post.title %></h1>
-            <% end %>
-          </div>
-          <%= unless is_nil(@post) do %>
-            <div class="metadata">
-              <div>
-                <div class="published">
-                  <%= Calendar.strftime(@post.published_at, "%B %d %Y, %H:%M:%S") %>
-                </div>
-                <div class="tags">
-                  <%= for tag <- @post.tags do %>
-                    <.button phx-click="tag" phx-value-tag={tag.label}>
-                      <%= "#" <> tag.label %>
-                    </.button>
-                  <% end %>
-                </div>
-              </div>
-              <div class="read_time">
-                Approx. <%= @post.reading_time_minutes %> minute read
+              <div class="tags">
+                <%= for tag <- @post.tags do %>
+                  <.button phx-click="tag" phx-value-tag={tag.label}>
+                    <%= "#" <> tag.label %>
+                  </.button>
+                <% end %>
               </div>
             </div>
-          <% end %>
+            <div class="post-published">
+              <%= Calendar.strftime(@post.published_at, "%B %d %Y, %H:%M:%S") %>
+            </div>
+            <div class="post-read-time">
+              Approx. <%= @post.reading_time_minutes %> minute read
+            </div>
+          </div>
 
           <article>
-            <%= if is_nil(@post) do %>
-              <p>It looks like something went wrong!! (｡•́︿•̀｡)</p>
-              <p>
-                The requested post does not seem to exist. It may have been deleted, renamed, or archived.
-                <br /><br />
-                If this is a mistake please feel free to reach out and I'll fix it in a jiffy!
-              </p>
-              <p>
-                <a phx-click="posts">See all posts&nbsp;</a> or <a phx-click="home">go home !!</a>
-              </p>
-            <% else %>
-              <%= {:safe, @post.body} %>
-            <% end %>
+            <%= {:safe, @post.body} %>
           </article>
         </main>
       <% end %>
