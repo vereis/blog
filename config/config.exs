9ce6bd423a7be869e0d5a29b21474e9491ff5235
vereis <me@vereis.com>
2025-06-19 10:46:01 +0100

    Add Genserver which polls lanyard for discord rich presence

--- config/config.exs
+++ config/config.exs
@@ -51,6 +51,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Configure Lanyard integration
+config :blog,
+  lanyard_discord_user_id: System.fetch_env!("LANYARD_DISCORD_USER_ID"),
+  lanyard_poll_interval: String.to_integer(System.fetch_env!("LANYARD_POLL_INTERVAL")) * 1000
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
