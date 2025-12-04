8daa42b55a7c98bc2a2228dfbd2a78e041c0d92b
vereis <me@vereis.com>
2025-12-04 22:28:07 +0000

    [FEAT] Create assets migration

---
+++ apps/blog/priv/repo/migrations/20251204215257_create_assets.exs
@@ -0,0 +1,20 @@
+defmodule Blog.Repo.Migrations.CreateAssets do
+  use Ecto.Migration
+
+  def change do
+    create table(:assets) do
+      add :name, :string, null: false
+      add :path, :string, null: false
+      add :data, :binary, null: false
+      add :width, :integer, null: false
+      add :height, :integer, null: false
+      add :content_type, :string, null: false
+      add :hash, :string
+
+      timestamps()
+    end
+
+    create unique_index(:assets, [:name])
+    create index(:assets, [:hash])
+  end
+end
