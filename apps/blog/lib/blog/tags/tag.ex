fc41831e1e7a504b87b77ebd020467c389a8819d
vereis <me@vereis.com>
2025-12-04 20:12:29 +0000

    [REFACTOR] Use join_schema and preload_put_assoc for many-to-many associations

--- apps/blog/lib/blog/tags/tag.ex
+++ apps/blog/lib/blog/tags/tag.ex
@@ -7,8 +7,13 @@ defmodule Blog.Tags.Tag do
   schema "tags" do
     field :label, :string
 
-    many_to_many :posts, Blog.Posts.Post, join_through: "posts_tags"
-    many_to_many :projects, Blog.Projects.Project, join_through: "projects_tags"
+    many_to_many :posts, Blog.Posts.Post,
+      join_through: join_schema("posts_tags", {:tag_id, :post_id}),
+      on_replace: :delete
+
+    many_to_many :projects, Blog.Projects.Project,
+      join_through: join_schema("projects_tags", {:tag_id, :project_id}),
+      on_replace: :delete
 
     timestamps()
   end
@@ -20,5 +25,7 @@ defmodule Blog.Tags.Tag do
     |> cast(attrs, @castable_fields)
     |> validate_required([:label])
     |> unique_constraint(:label)
+    |> preload_put_assoc(attrs, :posts, :post_ids)
+    |> preload_put_assoc(attrs, :projects, :project_ids)
   end
 end
