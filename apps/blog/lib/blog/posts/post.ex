22b49befb9672abf869783ebeed9c4cfe1f8bd38
vereis <me@vereis.com>
2025-12-04 11:06:59 +0000

    [FEAT] Add markdown processing, heading extraction, etc to Posts

--- apps/blog/lib/blog/posts/post.ex
+++ apps/blog/lib/blog/posts/post.ex
@@ -4,9 +4,19 @@ defmodule Blog.Posts.Post do
 
   import Ecto.Changeset
 
+  alias Blog.Markdown
+
   @type t :: %__MODULE__{}
 
   @castable_fields [:title, :raw_body, :slug, :is_draft, :published_at]
+  @slug_format ~r/^[a-z0-9_-]+$/
+  @reading_speed_wpm 260
+
+  defguardp valid?(changeset)
+            when is_struct(changeset, Ecto.Changeset) and changeset.valid? == true
+
+  defguardp changes?(changeset, field)
+            when is_struct(changeset, Ecto.Changeset) and is_map_key(changeset.changes, field)
 
   schema "posts" do
     field :title, :string
@@ -28,10 +38,74 @@ defmodule Blog.Posts.Post do
   end
 
   @doc false
+  @spec changeset(t() | Ecto.Changeset.t(t()), map()) :: Ecto.Changeset.t(t())
   def changeset(post, attrs) do
     post
     |> cast(attrs, @castable_fields)
     |> validate_required([:title, :raw_body, :slug])
+    |> validate_format(:slug, @slug_format, message: "must be lowercase alphanumeric with hyphens or underscores only")
     |> unique_constraint(:slug)
+    |> process_markdown()
+  end
+
+  defp calculate_reading_time(raw_body) do
+    word_count =
+      raw_body
+      |> String.split(~r/\s+/)
+      |> Enum.reject(&(&1 == ""))
+      |> length()
+
+    max(1, (word_count / @reading_speed_wpm) |> Float.ceil() |> trunc())
+  end
+
+  defp process_markdown(changeset) when not valid?(changeset) do
+    changeset
+  end
+
+  defp process_markdown(changeset) when not changes?(changeset, :raw_body) do
+    changeset
+  end
+
+  defp process_markdown(changeset) do
+    raw_body = get_change(changeset, :raw_body)
+
+    case Markdown.render(raw_body, &process_html/2) do
+      {:ok, [html, headings]} ->
+        reading_time = calculate_reading_time(raw_body)
+
+        changeset
+        |> put_change(:body, html)
+        |> put_change(:reading_time_minutes, reading_time)
+        |> put_embed(:headings, headings)
+
+      {:error, reason} ->
+        add_error(changeset, :raw_body, reason)
+    end
+  end
+
+  defp process_html({"h" <> level_str = tag, attrs, children}, acc) do
+    level = String.to_integer(level_str)
+    title = Floki.text({tag, attrs, children})
+    link = slugify_heading_text(title)
+
+    heading = %{
+      level: level,
+      title: title,
+      link: link
+    }
+
+    {{tag, [{"id", link} | attrs], children}, [heading | acc]}
+  end
+
+  defp process_html(other, acc) do
+    {other, acc}
+  end
+
+  defp slugify_heading_text(title) do
+    title
+    |> String.downcase()
+    |> String.replace(~r/[^\w\s-]/, "")
+    |> String.replace(~r/\s+/, "-")
+    |> String.trim("-")
   end
 end
