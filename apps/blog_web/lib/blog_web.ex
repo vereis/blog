d5fb2f576ee2fe624957e66a02757f7e1b043969
vereis <me@vereis.com>
2025-12-06 18:02:40 +0000

    [FEAT] Add XML format support to BlogWeb

--- apps/blog_web/lib/blog_web.ex
+++ apps/blog_web/lib/blog_web.ex
@@ -39,7 +39,7 @@ defmodule BlogWeb do
 
   def controller do
     quote do
-      use Phoenix.Controller, formats: [:html, :json]
+      use Phoenix.Controller, formats: [:html, :json, :xml]
 
       import Plug.Conn
 
@@ -72,6 +72,19 @@ defmodule BlogWeb do
         only: [get_csrf_token: 0, view_module: 1, view_template: 1]
 
       # Include general helpers for rendering HTML
+      unquote(html_helpers())
+    end
+  end
+
+  def xml do
+    quote do
+      use Phoenix.Component
+
+      # Import convenience functions from controllers
+      import Phoenix.Controller,
+        only: [get_csrf_token: 0, view_module: 1, view_template: 1]
+
+      # Include general helpers for rendering XML
       unquote(html_helpers())
     end
   end
