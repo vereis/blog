3bae148bd1f6e15e71f0a513d2a6bc8de5e4f82b
vereis <me@vereis.com>
2025-12-08 16:59:49 +0000

    [REFACTOR] Remove dead PageController and related files

--- apps/blog_web/lib/blog_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule BlogWeb.PageController do
-  use BlogWeb, :controller
-
-  def home(conn, _params) do
-    render(conn, :home)
-  end
-end
--- apps/blog_web/test/blog_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule BlogWeb.PageControllerTest do
-  use BlogWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Welcome to the blog!"
-  end
-end
