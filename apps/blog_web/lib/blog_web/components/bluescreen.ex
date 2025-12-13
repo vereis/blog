32cf6e051feb0890af8bcb558fbc13e7252c57e4
vereis <me@vereis.com>
2025-12-13 12:07:33 +0000

    [REFACTOR] Remove keypress hook from bluescreen component and use links instead

--- apps/blog_web/lib/blog_web/components/bluescreen.ex
+++ apps/blog_web/lib/blog_web/components/bluescreen.ex
@@ -23,51 +23,25 @@ defmodule BlogWeb.Components.Bluescreen do
     assigns = assign(assigns, :error_code, ErrorCode.generate(assigns.error))
 
     ~H"""
-    <div
-      class="bluescreen"
-      id="bluescreen"
-      phx-hook=".Bluescreen"
-      data-href={@href}
-      role="alert"
-      aria-label="Error message"
-    >
+    <div class="bluescreen" role="alert" aria-label="Error message">
       <div class="bluescreen-header">
         <Badge.badge class="bluescreen-badge">Unexpected Error</Badge.badge>
       </div>
       <div class="bluescreen-content">
         <p>An error has occurred. To continue:</p>
-        <p>Press <a href={@href}>Enter or Click</a> to return to the blog, or</p>
+        <p>
+          <.link navigate={@href}>Click here</.link> to return to the blog, or
+        </p>
         <p>
           Press CTRL+ALT+DEL to restart your computer. If you do this, you will lose any unsaved information in all open applications and it probably won't fix the problem (maybe just try again later).
         </p>
         <p class="bluescreen-error-code">Error: {@error_code}</p>
         <p class="bluescreen-prompt">
-          Press any key to continue<span class="bluescreen-cursor">█</span>
+          <.link navigate={@href}>Click to continue</.link>
+          <span class="bluescreen-cursor">█</span>
         </p>
       </div>
     </div>
-    <script :type={Phoenix.LiveView.ColocatedHook} name=".Bluescreen">
-      export default {
-        mounted() {
-          const href = this.el.dataset.href;
-          const handleKeyPress = (e) => {
-            window.location.href = href;
-          };
-          
-          document.addEventListener('keydown', handleKeyPress);
-          
-          this.cleanup = () => {
-            document.removeEventListener('keydown', handleKeyPress);
-          };
-        },
-        
-        destroyed() {
-          if (this.cleanup) {
-            this.cleanup();
-          }
-        }
-      }
-    </script>
     """
   end
 end
