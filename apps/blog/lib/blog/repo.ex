fadcf4b3a0a694d8e5e9f8cbe6c1b20f7590af82
vereis <me@vereis.com>
2025-12-30 12:36:07 +0000

    [CHORE] Clean up config comments and enable all-region LiteFS primary election

--- apps/blog/lib/blog/repo.ex
+++ apps/blog/lib/blog/repo.ex
@@ -5,14 +5,8 @@ defmodule Blog.Repo do
 
   use EctoMiddleware.Repo
 
-  @write_actions [:insert, :insert!, :update, :update!, :delete, :delete!, :insert_or_update, :insert_or_update!]
-
   @impl EctoMiddleware.Repo
-  def middleware(action, _resource) when action in @write_actions do
-    [EctoLiteFS.Middleware]
-  end
-
   def middleware(_action, _resource) do
-    []
+    [EctoLiteFS.Middleware]
   end
 end
