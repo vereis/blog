27fc790e0093b7cfeec3ff88a3256c73eb7f5997
vereis <me@vereis.com>
2026-01-06 17:16:42 +0000

    [FEAT] Simplify Link schema using Ecto.Enum and improve docs

--- apps/blog/lib/blog/content/link.ex
+++ apps/blog/lib/blog/content/link.ex
@@ -4,28 +4,45 @@ defmodule Blog.Content.Link do
 
   Links are stored without foreign key constraints to allow broken links.
   The context field distinguishes between body wikilinks and tag associations.
+
+  ## Examples
+
+  ### Body wikilinks (from markdown)
+      
+      # In markdown file
+      Check out [[essays/other-post]] for more details.
+
+      # Stored as
+      %Link{
+        source_slug: "essays/my-post",
+        target_slug: "essays/other-post",
+        context: "body"
+      }
+
+  ### Tag references (from frontmatter)
+
+      # In frontmatter
+      tags:
+        - tags/elixir
+        - tags/testing
+
+      # Stored as
+      %Link{source_slug: "essays/my-post", target_slug: "tags/elixir", context: "tag"}
+      %Link{source_slug: "essays/my-post", target_slug: "tags/testing", context: "tag"}
   """
   use Blog.Schema
 
-  @type context :: :body | :tag
-
-  @contexts [:body, :tag]
+  import Ecto.Changeset
 
   @primary_key false
 
   schema "content_links" do
     field :source_slug, :string, primary_key: true
     field :target_slug, :string, primary_key: true
-    field :context, :string, primary_key: true
+    field :context, Ecto.Enum, values: [:body, :tag], primary_key: true
 
     timestamps()
   end
-
-  @doc """
-  Returns the list of valid link contexts.
-  """
-  @spec contexts() :: [context()]
-  def contexts, do: @contexts
 
   @doc false
   @spec changeset(t() | Ecto.Changeset.t(t()), map()) :: Ecto.Changeset.t(t())
@@ -33,27 +50,14 @@ defmodule Blog.Content.Link do
     link
     |> cast(attrs, [:source_slug, :target_slug, :context])
     |> validate_required([:source_slug, :target_slug, :context])
-    |> validate_inclusion(:context, Enum.map(@contexts, &Atom.to_string/1))
     |> unique_constraint([:source_slug, :target_slug, :context], name: :content_links_pkey)
   end
 
   @impl EctoUtils.Queryable
-  def base_query(queryable \\ __MODULE__), do: queryable
-
-  @impl EctoUtils.Queryable
   def query(base_query, filters) do
     Enum.reduce(filters, base_query, fn
-      {:source_slug, slug}, query when is_binary(slug) ->
-        from link in query, where: link.source_slug == ^slug
-
-      {:target_slug, slug}, query when is_binary(slug) ->
-        from link in query, where: link.target_slug == ^slug
-
-      {:context, context}, query when is_atom(context) ->
-        from link in query, where: link.context == ^Atom.to_string(context)
-
-      {:context, context}, query when is_binary(context) ->
-        from link in query, where: link.context == ^context
+      {:context, context}, query ->
+        from link in query, where: link.context == ^to_string(context)
 
       {key, value}, query ->
         EctoUtils.Queryable.apply_filter(query, key, value)
